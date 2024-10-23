cad26a85bd70cecbbe185edbd0f6c230a0abf7d6
esantos2019254 <esantos-2019254@kinal.edu.gt>
2024-10-22 21:41:17 -0600

    correccion de error

--- src/main/java/com/elmer/system/NameController.java
+++ src/main/java/com/elmer/system/NameController.java
@@ -1,7 +1,9 @@
 package com.elmer.system;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class NameController {
 
     @GetMapping("/nombre")
